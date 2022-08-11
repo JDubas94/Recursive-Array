efacbc17962d81978cc4244cabc962bf7dedae57
Julia Dubas <uliadubas94@gmail.com>
2022-08-11 23:13:36 +0300

    Add project files.

---
+++ recursive array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace recursive_array
+{
+    class Program
+    {
+        static void RecursiveArray(int[] array, int index)
+        {
+
+            if (index >= array.Length)
+                return;
+
+            Console.WriteLine(array[index]);
+
+            index++;
+
+
+            RecursiveArray(array, index);
+        }
+
+        static void Main(string[] args)
+        {
+             int[] myArray = new[] { 2, 2, 3, 4, 6, 7, 10};
+
+            RecursiveArray(myArray, 4);
+
+            Console.ReadLine();
+        }
+    }
+}
